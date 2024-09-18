991a78edf292b95805856abc3380a7be33c4c6d1
pxb123go <sahoopradyun@gmail.com>
2024-09-18 20:12:04 +0300

    initial commit

---
+++ calci.java
@@ -0,0 +1,14 @@
+class calci {
+    public static void main(String[] args){
+        int num1=10;
+        int num2=5;
+        int sum=num1+num2;
+        int sum2=num1*num2;
+        int sum3=num1/num2;
+        int sum4=num1-num2;
+        System.out.println("The sum of 10 and 5 is: "+ sum);
+        System.out.println("The product of 10 and 5 is: "+ sum2);
+        System.out.println("The quotient of 10 and 5 is: "+ sum3);
+        System.out.println("The differnce of 10 and 5 is: "+ sum4);
+    }
+    }
